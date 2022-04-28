a215c54ce5ac324159362d5aea3c0fd5f75fe145
kkiirriillll <103149973+kkiirriillll@users.noreply.github.com>
2022-04-28 16:54:11 +0300

    Add files via upload

---
+++ arp.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+arp -i eth0 >> /home/kirill/arp.txt;
+echo -- >> /home/kirill/arp.txt ;
+while (true)
+do
+sleep 1m;
+arp -i eth0 | grep enp0s3 >> /home/kirill/arp.txt;
+echo -- >> /home/kirill/arp.txt ;
+done;
